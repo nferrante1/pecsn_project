df79a9881037b4b27470d6beb8fdf06de61c22aa
nferrante1 <nicolaferrante96@gmail.com>
2019-12-03 19:44:13 +0100

    First commit:

---
+++ src/remote_server.h
@@ -0,0 +1,37 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __PECSN_PROJECT_REMOTE_SERVER_H_
+#define __PECSN_PROJECT_REMOTE_SERVER_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+namespace pecsn_project {
+
+/**
+ * TODO - Generated class
+ */
+class Remote_server : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+} //namespace
+
+#endif
